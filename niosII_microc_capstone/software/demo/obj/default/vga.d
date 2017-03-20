8af003ac197429fad03e7b100f3d3ca3ce9e4397
Scott Wowk <sdwowk@ualberta.ca>
2017-03-20 11:00:30 -0600

    Initial commit

---
+++ niosII_microc_capstone/software/demo/obj/default/vga.d
@@ -0,0 +1,61 @@
+obj/default/vga.o: vga.c ../demo_bsp//HAL/inc/includes.h \
+  ../demo_bsp//HAL/inc/os_cpu.h ../demo_bsp//HAL/inc/sys/alt_irq.h \
+  ../demo_bsp//HAL/inc/nios2.h ../demo_bsp//HAL/inc/alt_types.h \
+  ../demo_bsp/system.h ../demo_bsp/linker.h \
+  ../demo_bsp//UCOSII/inc/os_cfg.h ../demo_bsp//HAL/inc/sys/alt_alarm.h \
+  ../demo_bsp//HAL/inc/sys/alt_llist.h \
+  ../demo_bsp//HAL/inc/priv/alt_alarm.h ../demo_bsp//HAL/inc/alt_types.h \
+  ../demo_bsp/system.h ../demo_bsp//UCOSII/inc/ucos_ii.h \
+  ../demo_bsp//UCOSII/inc/os_cfg.h ../demo_bsp//HAL/inc/os_cpu.h \
+  ../demo_bsp//drivers/inc/altera_up_sd_card_avalon_interface.h \
+  ../demo_bsp//HAL/inc/alt_types.h ../demo_bsp//HAL/inc/sys/alt_dev.h \
+  ../demo_bsp//HAL/inc/sys/alt_llist.h \
+  ../demo_bsp//HAL/inc/priv/alt_dev_llist.h \
+  ../demo_bsp//HAL/inc/sys/alt_llist.h \
+  ../demo_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h
+
+../demo_bsp//HAL/inc/includes.h:
+
+../demo_bsp//HAL/inc/os_cpu.h:
+
+../demo_bsp//HAL/inc/sys/alt_irq.h:
+
+../demo_bsp//HAL/inc/nios2.h:
+
+../demo_bsp//HAL/inc/alt_types.h:
+
+../demo_bsp/system.h:
+
+../demo_bsp/linker.h:
+
+../demo_bsp//UCOSII/inc/os_cfg.h:
+
+../demo_bsp//HAL/inc/sys/alt_alarm.h:
+
+../demo_bsp//HAL/inc/sys/alt_llist.h:
+
+../demo_bsp//HAL/inc/priv/alt_alarm.h:
+
+../demo_bsp//HAL/inc/alt_types.h:
+
+../demo_bsp/system.h:
+
+../demo_bsp//UCOSII/inc/ucos_ii.h:
+
+../demo_bsp//UCOSII/inc/os_cfg.h:
+
+../demo_bsp//HAL/inc/os_cpu.h:
+
+../demo_bsp//drivers/inc/altera_up_sd_card_avalon_interface.h:
+
+../demo_bsp//HAL/inc/alt_types.h:
+
+../demo_bsp//HAL/inc/sys/alt_dev.h:
+
+../demo_bsp//HAL/inc/sys/alt_llist.h:
+
+../demo_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../demo_bsp//HAL/inc/sys/alt_llist.h:
+
+../demo_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h:
